9a216281c71e747fd9b2c54e27fe1a01ff27b57d
Renier Trenuela <rtrenuela@objectbright.com>
2020-11-16 18:10:45 +0800

    menu fix

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -41,12 +41,12 @@
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
         <a class="navbar-brand js-scroll-trigger" href="{{ route('landing') }}">Tabang Calamity</a>
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-                data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
-                aria-label="Toggle navigation">
-            Menu
-            <i class="fas fa-bars"></i>
-        </button>
+{{--        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"--}}
+{{--                data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"--}}
+{{--                aria-label="Toggle navigation">--}}
+{{--            Menu--}}
+{{--            <i class="fas fa-bars"></i>--}}
+{{--        </button>--}}
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
 {{--                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="{{ route('form') }}">Tabang Form</a></li>--}}
