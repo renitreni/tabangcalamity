14b9b8d176d1026385fba5288555f99d89082f97
Mohammed <Aziz.Mohammed@Aziz-Mohammed.local>
2022-01-06 04:42:43 +0800

    hadji:update

--- public/js/app.js
+++ public/js/app.js
@@ -515,8 +515,8 @@ eval("// removed by extract-text-webpack-plugin//# sourceURL=[module]\n//# sourc
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/mac-user/www/tabangcalamity/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/mac-user/www/tabangcalamity/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/Aziz.Mohammed/www/tabangcalamity/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/Aziz.Mohammed/www/tabangcalamity/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
